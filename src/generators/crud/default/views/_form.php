bcb01eaf4f358bbe4ecc87a084b217f9a8cd394c
Azamat <azamat.eldarhanov@chulakov.ru>
2019-09-03 16:09:40 +0300

    Добавлен вывод полей изображения и цвета

--- src/generators/crud/default/views/_form.php
+++ src/generators/crud/default/views/_form.php
@@ -10,6 +10,7 @@
  */
 
 echo "<?php\n";
+
 ?>
 /**
  * Файл шаблона формы
@@ -23,6 +24,8 @@ echo "<?php\n";
  */
 
 use yii\helpers\Html;
+<?php echo in_array('color', array_column($properties, 'type')) ? 'use kartik\color\ColorInput;' : ''; ?>
+<?php echo in_array('Image', array_column($properties, 'type')) ? 'use chulakov\fileinput\widgets\FileInput;' : ''; ?>
 
 ?>
 
@@ -37,7 +40,29 @@ use yii\helpers\Html;
 ?>
 <div class="row">
     <div class="col-md-12">
+<?php if ($attribute['type'] == 'color'): ?>
+        <?= "<?=" ?> $form->field($model, '<?= $attribute['name']; ?>')->widget(ColorInput::class, [
+            'options' => ['placeholder' => \Yii::t('ch/all', 'Select color...')],
+        ]); ?>
+<?php elseif ($attribute['type'] == 'Image'): ?>
+        <?= "<?=" ?> $form->field($model, '<?= $attribute['name']; ?>')->widget(FileInput::class, [
+            'options' => [
+                'multiple' => false
+            ],
+            'attachedFilesAttribute' => '<?= $attribute['name']; ?>Attached',
+            'pluginOptions' => [
+                'overwriteInitial' => true,
+                'showUpload' => false,
+                'showClose' => false,
+                'showRemove' => false,
+                'fileActionSettings' => [
+                    'showRemove' => false,
+                ],
+            ],
+        ]); ?>
+<?php else: ?>
         <?= "<?= " . $generator->generateActiveField($attribute['name']) . "; ?>\n"; ?>
+<?php endif; ?>
     </div>
 </div>
 <?php endforeach; ?>
